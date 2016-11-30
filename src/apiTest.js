3667e64e84645bce3f73ba57e9c4507ec2c6712c
Max Horstmann <mhorstmann@stackoverflow.com>
2016-11-30 16:44:45 -0500

    still trying to figure out gzip / node..

--- src/apiTest.js
+++ src/apiTest.js
@@ -1,10 +1,10 @@
-// var request = require('request');
-// request('https://api.stackexchange.com/2.2/sites', function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       //var info = JSON.parse(body);
-//       console.log(body);
-//     }
-// })
+var request = require('request');
+request('https://api.stackexchange.com/2.2/sites?key=dzqlqab4VD4bynFom)Z1Ng((', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      console.log(body);
+    }
+})
 
 
 var request = require('request'), zlib = require('zlib');
@@ -13,8 +13,17 @@ var headers = {
   'Accept-Encoding': 'gzip'
 };
 
-request({url:'https://api.stackexchange.com/2.2/sites', 'headers': headers})
-    .pipe(zlib.createGunzip()) // unzip
-    .pipe(process.stdout); // do whatever you want with the stream
+var body = '';
+request({url:'https://api.stackexchange.com/2.2/sites?key=dzqlqab4VD4bynFom)Z1Ng((', headers: headers})
+    .pipe(zlib.createGunzip()    // unzip
+    .on("data", function(buffer) {
+    	console.log(buffer);
+    	//var part = buffer.read().toString();
+    	//body += part;
+    })
+    .on("end", function() {
+    	console.log("done!");
+	    var foo = JSON.parse(body);
+	    console.log(foo);
+    }));
 
-    
