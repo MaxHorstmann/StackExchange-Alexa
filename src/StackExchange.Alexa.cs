8c035468dec513ab89b900d3b6620efa0ea65ec2
Max Horstmann <mhorstmann@stackoverflow.com>
2017-06-17 20:05:05 -0400

    new version

---
+++ src/StackExchange.Alexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace StackExchange.Alexa
+{
+    public class Service
+    {
+    	// TODO 
+    	public object GetResponse()
+    	{
+    		return new { message = "Hello from Lambda!", time = DateTime.UtcNow };
+    	}
+    
+}
