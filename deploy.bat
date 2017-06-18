ba132f45084d35f5de3dd129bfc5f33d1872739c
Max Horstmann <mhorstmann@stackoverflow.com>
2017-06-18 17:35:17 -0400

    remove buildspec.yml, let's just deploy from local for now

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 cd src
 dotnet restore
-dotnet lambda deploy-function --region us-east-1 -c Release -f netcoreapp1.0 -frun dotnetcore1.0 -fh MyProject::getStackExchange.Alexa.Service::GetResponse -fn StackExchangeAlexaResponse
+dotnet lambda deploy-function --region us-east-1 -c Release -f netcoreapp1.0 -frun dotnetcore1.0 -fh MyProject::getStackExchange.Alexa.Service::GetResponse -fn StackExchangeAlexaResponse -fms 512 -ft 15 -frole myLambdaRole
 cd ..
