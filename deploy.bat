58f76e7835c8b7a58a82643bb027c1b5e88d90ce
Max Horstmann <mhorstmann@stackoverflow.com>
2017-06-23 21:00:54 -0400

    add scaffolding for alexa function

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 cd src
 dotnet restore 
-dotnet lambda deploy-function --region us-east-1 -c Release -f netcoreapp1.0 -frun dotnetcore1.0 -fh StackExchange.Alexa::StackExchange.Alexa.Service::GetResponse -fn StackExchangeAlexaResponse -fms 512 -ft 15 -frole LambdaRole
+dotnet lambda deploy-function --region us-east-1 -c Release -f netcoreapp1.1 -frun dotnetcore1.1 -fh StackExchange.Alexa::StackExchange.Alexa.Service::GetResponse -fn StackExchangeAlexaResponse -fms 512 -ft 15 -frole LambdaRole
 cd ..
--- src/StackExchange.Alexa.cs
+++ src/StackExchange.Alexa.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Amazon.Lambda.Core;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Alexa.NET.Request.Type;
+using Newtonsoft.Json;
+
 
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -7,11 +12,85 @@ namespace StackExchange.Alexa
 {
     public class Service
     {
-    	// TODO 
-    	public object GetResponse()
-    	{
-    		return new { message = "Hello from Lambda!", time = DateTime.UtcNow };
-    	}
+    	public SkillResponse GetResponse(SkillRequest input, ILambdaContext context)
+        {
+            var log = context.Logger;
+            try
+            {
+                SkillResponse response = new SkillResponse();
+                response.Response = new ResponseBody();
+                response.Response.ShouldEndSession = false;
+                IOutputSpeech innerResponse = null;
+                log.LogLine($"Skill Request Object:");
+                log.LogLine(JsonConvert.SerializeObject(input));
+
+                if (input.GetRequestType() == typeof(LaunchRequest))
+                {
+                    log.LogLine($"Default LaunchRequest made: 'Alexa, open Stack Exchange");
+                    innerResponse = new PlainTextOutputSpeech()
+                    {
+                        Text = "Welcome to Stack Exchange!"
+                    };
+                }
+                else if (input.GetRequestType() == typeof(IntentRequest))
+                {
+                    var intentRequest = (IntentRequest)input.Request;
+
+                    switch (intentRequest.Intent.Name)
+                    {
+                        case "InboxIntent":
+                            log.LogLine($"Inbox intent");
+                            innerResponse = new PlainTextOutputSpeech();
+                            (innerResponse as PlainTextOutputSpeech).Text = "Your inbox is empty.";
+                            break;
+                        case "AMAZON.CancelIntent":
+                            log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                            innerResponse = new PlainTextOutputSpeech();
+                            //(innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+                            response.Response.ShouldEndSession = true;
+                            break;
+                        case "AMAZON.StopIntent":
+                            log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                            innerResponse = new PlainTextOutputSpeech();
+                            //(innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+                            response.Response.ShouldEndSession = true;
+                            break;
+                        case "AMAZON.HelpIntent":
+                            log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                            innerResponse = new PlainTextOutputSpeech();
+                            //(innerResponse as PlainTextOutputSpeech).Text = resource.HelpMessage;
+                            break;
+                        default:
+                            log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                            innerResponse = new PlainTextOutputSpeech();
+                            //(innerResponse as PlainTextOutputSpeech).Text = resource.HelpReprompt;
+                            break;
+                    }
+                }
+
+                response.Response.OutputSpeech = innerResponse;
+                response.Version = "1.0";
+
+                if (response.SessionAttributes == null)
+                {
+                    response.SessionAttributes = new System.Collections.Generic.Dictionary<string, object>();
+                }
+                //response.SessionAttributes.Add("foo", count++);
+
+                log.LogLine($"Skill Response Object...");
+                log.LogLine(JsonConvert.SerializeObject(response));
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                log.LogLine("Unhandled exception:");
+                log.LogLine(ex.ToString());
+                log.LogLine(JsonConvert.SerializeObject(ex));
+                throw;
+            }
+        }
+
     }
     
 }
