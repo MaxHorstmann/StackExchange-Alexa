d49c2147e3baf945a96c16209e54fe13c46c513c
Max Horstmann <mhorstmann@stackoverflow.com>
2017-07-19 22:08:48 -0400

    record MixPanel events

--- src/StackExchange.Alexa.Service.cs
+++ src/StackExchange.Alexa.Service.cs
@@ -9,6 +9,7 @@ using Alexa.NET.Response;
 using Alexa.NET.Request.Type;
 using Newtonsoft.Json;
 using StackExchange.API;
+using Mixpanel;
 
 
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -18,6 +19,7 @@ namespace StackExchange.Alexa
     public partial class Service
     {
       	const string StackApiKey = "dzqlqab4VD4bynFom)Z1Ng(("; // not a secret
+        const string MixPanelToken = "<token>"; // this one IS a secret
 
     	private class State
     	{
@@ -28,11 +30,17 @@ namespace StackExchange.Alexa
     	private Client _client;
     	private ILambdaContext _context;
     	private State _state;
+        private MixpanelClient _mixpanel;
 
     	// Main entry point
     	public async Task<SkillResponse> GetResponse(SkillRequest request, ILambdaContext context)
         {
             context.Logger.LogLine(JsonConvert.SerializeObject(request));
+            MixpanelConfig.Global.SerializeJsonFn = JsonConvert.SerializeObject;            
+            _mixpanel = new MixpanelClient(MixPanelToken);
+            
+            await _mixpanel.TrackAsync("GetResponse.Begin", new {});
+
             try
             {
             	_context = context;
@@ -40,10 +48,12 @@ namespace StackExchange.Alexa
 				_client = new Client(StackApiKey, request?.Session?.User?.AccessToken); // StackExchange API client
 				var response = await RouteRequest(request);
 	            context.Logger.LogLine(JsonConvert.SerializeObject(response));
+                await _mixpanel.TrackAsync("GetResponse.End", new {});
 				return response;
             }
             catch (Exception ex)
             {
+                await _mixpanel.TrackAsync("GetResponse.Exception", new {});
                 context.Logger.LogLine("Unhandled exception:");
                 context.Logger.LogLine(ex.ToString());
                 context.Logger.LogLine(JsonConvert.SerializeObject(ex));
@@ -64,10 +74,15 @@ namespace StackExchange.Alexa
 
         public async Task<SkillResponse> RouteRequest(SkillRequest input)
     	{
-            if (input.GetRequestType() == typeof(LaunchRequest)) return await GetLaunchRequestResponse();
+            if (input.GetRequestType() == typeof(LaunchRequest)) 
+            {
+                await _mixpanel.TrackAsync("Launch", new {});
+                return await GetLaunchRequestResponse();
+            }
             if (input.GetRequestType() == typeof(IntentRequest))
             {
             	var intentRequest = (IntentRequest)input.Request;
+                await _mixpanel.TrackAsync("Intent", new { Name = intentRequest.Intent.Name });
             	if (intentRequest.Intent.Name=="InboxIntent") return await GetInboxIntentResponse();
             	if (intentRequest.Intent.Name=="HotQuestionIntent") return await GetHotQuestionIntentResponse();
             	if (intentRequest.Intent.Name=="HotQuestionDetailsIntent") return await GetHotQuestionDetailsIntentResponse();
